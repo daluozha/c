5307d4050c40e17ff79e4cd4e3260e98be6d8092
daluozha <561890199@qq.com>
2021-03-01 11:04:32 +0800

    selection sort

--- c-sort-algorithm/selection_sort.c
+++ c-sort-algorithm/selection_sort.c
@@ -1,17 +1,27 @@
 #include <stdio.h>
+/*  
+举个例子：
+    军训排队的时候，教官说：“你们都站着别动，我看谁个子最小。”
+然后教官选出个子最小的同学，说：“第一个位置是你的了，你和第一个同学换一下，剩下的同学我继续选。”
+*/
+
+// 函数定义
 void selection_sort(int arr[],int len){
     int i,j,temp;
-    for(i=0;i<len-1;i++){
+    for(i = 0;i < len-1;i++){
         int min = i;
-        for(j=i+1;j<len;j++) if(arr[min]>arr[j]) min=j;
-        if(min!=i){
-            temp=arr[min];
-            arr[min]=arr[i];
-            arr[i]=temp;
+        for(j = i+1;j < len;j++) if(arr[min] > arr[j]) min = j;
+        if(min != i){
+            temp = arr[min];
+            arr[min] = arr[i];
+            arr[i] = temp;
         }
         /* swap(&a[min], &a[i]);  */   // 使用自定义函数交換
     }
 }
+
+// 测试流程
+
 /*
 void swap(int *a,int *b) // 交换两个变量
 {
