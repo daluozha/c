31555f3876f1447507ba47052095e51ed95cd094
daluozha <561890199@qq.com>
2021-03-01 14:37:29 +0800

    insertion sort

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # c
-folder
+## folder
 - c-sort-algorithm  <https://www.runoob.com/cprogramming/c-sort-algorithm.html >
 - c-examples  <https://www.runoob.com/cprogramming/c-examples.html >
 - c-100-examples  <https://www.runoob.com/cprogramming/c-100-examples.html >
@@ -9,3 +9,6 @@ folder
 - c在线运行工具：<https://c.runoob.com/compile/11 >
 - VSCode插件：Code Runner
 - 直接双击同名无后缀文件
+
+## 相关资源
+- <https://www.runoob.com/w3cnote/ten-sorting-algorithm.html >
---
+++ c-sort-algorithm/insertion_sort.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/*  
+举个例子：
+    军训排队在已经排好了一个纵队的时候，有人要临时加入到这个队里来.
+于是教官大声喊道：“新来的，迅速找到你的位置，入队！”于是新来的插入到这个队伍的合适位置中。
+*/
+
+// 函数定义
+void insertion_sort(int arr[], int len){
+    int i,j,tmp;
+    for(i = 1;i < len;i++){
+        tmp = arr[i];
+        for(i = j;j > 0 && arr[j - 1] > tmp;j--){
+            arr[j] = arr[j - 1];
+        }
+        arr[j] = tmp;
+    }
+}
+
+// 测试流程
+
+int main() {
+    int arr[] = { 22, 34, 3, 32, 82, 55, 89, 50, 37, 5, 64, 35, 9, 70 };
+    int len = (int) sizeof(arr) / sizeof(*arr);
+    insertion_sort(arr, len);
+    for (int i = 0; i < len; i++)
+        printf("%d ", arr[i]);
+    return 0;
+}
